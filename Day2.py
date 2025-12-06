749956ee9f48221451eef73fe55634c5852b18d6
sathishsuresh1 <sathishsuresh1@outlook.com>
2025-12-06 10:22:37 +0400

    add day 2 file

---
+++ Day2.py
@@ -0,0 +1,25 @@
+a = "Sathish is an AI Enigineer"
+b = ['S', 'a', 't', 'h', 'i', 's', 'h']
+c = (1, 2, 3, 4, 5) 
+d = {'name': 'Sathish', 'role': 'AI Engineer'}  
+e = {10, 20, 30, 40, 50}
+f = {'key1': 'value1', 'key2': 'value2'}
+
+print("String a:", a)
+print("List b:", b)
+print("Tuple c:", c)
+print("Dictionary d:", d)
+print("Set e:", e)
+print("Frozen Set f:", f)
+#Data Types 
+print("Type of a:", type(a))
+print("Type of b:", type(b))
+print("Type of c:", type(c))
+print("Type of d:", type(d))
+print("Type of e:", type(e))
+print("Type of f:", type(f))
+
+a = 25
+print("Value of a:", a)
+b = [1]
+print("Value of b:", b)
